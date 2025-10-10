667de95a7c0f4bb28b26369774e77bb8d1fff6bf
lekuro <lekurot@gmail.com>
2025-10-10 09:13:32 +0300

    promiseAll function work now

--- lesson06-JS-try-catch-async-await/src/promise-all.js
+++ lesson06-JS-try-catch-async-await/src/promise-all.js
@@ -2,35 +2,32 @@
 
 import { openApiUrls } from './models/index.js';
 
-async function getApiPromise(url) {
-    return await fetch(url);
-}
+const responses = await Promise.all(openApiUrls.map((url) => fetch(url)));
+responses.forEach((response) => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status} for URL: ${response.url}`);
+    }
+    const jsonData = response.json();
+    console.log('-------- response --------');
+    console.log(response);
+    console.log('-------- response.status --------');
+    console.log(response.status);
+    console.log('-------- response.url --------');
+    console.log(response.url);
+    console.log('-------- response.ok --------');
+    console.log(response.ok);
+    console.log('-------- jsonData --------');
+    console.log(jsonData);
+});
 
-const arrayOfOpenApiPromises = openApiUrls.map((url) => getApiPromise(url));
-console.log(arrayOfOpenApiPromises);
-console.log(openApiUrls);
-const arrayOfOpenApiPromisesResponse = await Promise.all(arrayOfOpenApiPromises)
-    .then((responses) => {
-        responses.forEach((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status} for URL: ${response.url}`);
-            }
-            console.log('-------- response --------');
-            console.log(response.json());
-            console.log('-------- response.status --------');
-            console.log(response.status);
-            console.log('-------- response.url --------');
-            console.log(response.url);
-            console.log('-------- response.ok --------');
-            console.log(response.ok);
-        });
-        return responses.json();
-    })
-    .catch((error) => {
-        console.log('-------- error --------');
-        console.log(error);
-    })
-    .finally(() => {
-        console.log('-------- finally --------');
-    });
-console.log(arrayOfOpenApiPromisesResponse);
+// const [users, posts, comments] = await Promise.all([
+//     fetch(openApiUrls[0]),
+//     fetch(openApiUrls[2]),
+//     fetch(openApiUrls[3])
+// ]);
+// console.log('-------- users --------');
+// console.log(users);
+// console.log('-------- posts --------');
+// console.log(posts);
+// console.log('-------- comments --------');
+// console.log(comments);
