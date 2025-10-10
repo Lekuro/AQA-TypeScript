f0292b6b4c1d1dff2ae1a99c93c29e784c50e285
lekuro <lekurot@gmail.com>
2025-10-10 10:14:24 +0300

    do not understand how to get json() data from promiseAll

--- lesson06-JS-try-catch-async-await/src/promise-all.js
+++ lesson06-JS-try-catch-async-await/src/promise-all.js
@@ -5,20 +5,24 @@ import { openApiUrls } from './models/index.js';
 console.log('-------- Promise.all together --------');
 const responses = await Promise.all(openApiUrls.map((url) => fetch(url)));
 responses.forEach((response) => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status} for URL: ${response.url}`);
+    try {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status} for URL: ${response.url}`);
+        }
+        console.log('-------- response --------');
+        console.log(response);
+        console.log('-------- response.ok --------');
+        console.log(response.ok);
+        console.log('-------- response.status --------');
+        console.log(response.status);
+        console.log('-------- response.url --------');
+        console.log(response.url);
+        console.log('-------- response.json --------');
+        console.log(response.json());
+    } catch (error) {
+        console.log('-------- error --------');
+        console.log(error);
     }
-    const jsonData = response.json();
-    console.log('-------- response --------');
-    console.log(response);
-    console.log('-------- response.status --------');
-    console.log(response.status);
-    console.log('-------- response.url --------');
-    console.log(response.url);
-    console.log('-------- response.ok --------');
-    console.log(response.ok);
-    console.log('-------- jsonData --------');
-    console.log(jsonData);
 });
 
 console.log('-------- Promise.all separately --------');
