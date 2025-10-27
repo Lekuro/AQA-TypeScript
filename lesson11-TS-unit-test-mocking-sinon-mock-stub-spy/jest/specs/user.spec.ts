46b585264ee633a5148397d13f525bfacae1df19
lekuro <lekurot@gmail.com>
2025-10-27 17:43:17 +0200

    clean file, add other user

--- lesson11-TS-unit-test-mocking-sinon-mock-stub-spy/jest/specs/user.spec.ts
+++ lesson11-TS-unit-test-mocking-sinon-mock-stub-spy/jest/specs/user.spec.ts
@@ -1,33 +1,33 @@
 import { jest } from '@jest/globals';
 import { ApiPostsDto, ApiTodosDto, ApiUserClass, ApiUserPostsTodos, ApiUsersDto, ApiUserWithPostsAndTodos } from '../src/models/index';
-import { todosOfUser3 } from './todos-of-user3';
-import { postsOfUser3 } from './posts-of-user3';
+import { todosOfUser3 } from '../src/data/todos-of-user3';
+import { postsOfUser3 } from '../src/data/posts-of-user3';
 
-describe('-------- User unit tests --------', () => {
+const userObj: ApiUserClass = {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    address: {
+        street: 'Douglas Extension',
+        suite: 'Suite 847',
+        city: 'McKenziehaven',
+        zipcode: '59590-4157',
+        geo: { lat: '-68.6102', lng: '-47.0653' }
+    },
+    phone: '1-463-123-4447',
+    website: 'ramiro.info',
+    company: {
+        name: 'Romaguera-Jacobson',
+        catchPhrase: 'Face to face bifurcated interface',
+        bs: 'e-enable strategic applications'
+    }
+};
+
+describe('-------- ApiUserWithPostsAndTodos unit tests --------', () => {
     let user: ApiUserWithPostsAndTodos;
     const posts: ApiPostsDto[] = postsOfUser3;
     const todos: ApiTodosDto[] = todosOfUser3;
-
-    const userObj: ApiUserClass = {
-        id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net',
-        address: {
-            street: 'Douglas Extension',
-            suite: 'Suite 847',
-            city: 'McKenziehaven',
-            zipcode: '59590-4157',
-            geo: { lat: '-68.6102', lng: '-47.0653' }
-        },
-        phone: '1-463-123-4447',
-        website: 'ramiro.info',
-        company: {
-            name: 'Romaguera-Jacobson',
-            catchPhrase: 'Face to face bifurcated interface',
-            bs: 'e-enable strategic applications'
-        }
-    };
 
     beforeEach(() => {
         // Arrange
@@ -62,5 +62,56 @@ describe('-------- User unit tests --------', () => {
         expect(user.getTodos).toHaveReturnedTimes(1);
         expect(user.getTodos).toHaveReturned();
         expect(user.todos).toEqual(todos);
+    });
+});
+
+describe('-------- ApiUserPostsTodos unit tests --------', () => {
+    let user: ApiUserPostsTodos;
+
+    beforeEach(() => {
+        // Arrange
+        user = new ApiUserPostsTodos(3);
+
+        jest.spyOn(user, 'getUser');
+        jest.spyOn(user, 'getPosts');
+        jest.spyOn(user, 'getTodos');
+    });
+
+    afterEach(() => {
+        // jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
+    it('should be called getUser method', async () => {
+        // Act
+        await user.getUser();
+        // Assert
+        expect(user.getUser).toHaveBeenCalled();
+        expect(user.getUser).toHaveBeenCalledTimes(1);
+        expect(user.getUser).toHaveReturnedTimes(1);
+        expect(user.getUser).toHaveReturned();
+        expect(user.user).toEqual(userObj);
+    });
+
+    it('should be called getPosts method', async () => {
+        // Act
+        await user.getPosts();
+        // Assert
+        expect(user.getPosts).toHaveBeenCalled();
+        expect(user.getPosts).toHaveBeenCalledTimes(1);
+        expect(user.getPosts).toHaveReturnedTimes(1);
+        expect(user.getPosts).toHaveReturned();
+        expect(user.posts).toEqual(postsOfUser3);
+    });
+
+    it('should be called getTodos method', async () => {
+        // Act
+        await user.getTodos();
+        // Assert
+        expect(user.getTodos).toHaveBeenCalled();
+        expect(user.getTodos).toHaveBeenCalledTimes(1);
+        expect(user.getTodos).toHaveReturnedTimes(1);
+        expect(user.getTodos).toHaveReturned();
+        expect(user.todos).toEqual(todosOfUser3);
     });
 });
---
+++ lesson11-TS-unit-test-mocking-sinon-mock-stub-spy/jest/src/data/posts-of-user3.ts
@@ -0,0 +1,102 @@
+export const postsOfUser3 = [
+    {
+        userId: 3,
+        id: 21,
+        title: 'asperiores ea ipsam voluptatibus modi minima quia sint',
+        body:
+            'repellat aliquid praesentium dolorem quo\n' +
+            'sed totam minus non itaque\n' +
+            'nihil labore molestiae sunt dolor eveniet hic recusandae veniam\n' +
+            'tempora et tenetur expedita sunt'
+    },
+    {
+        userId: 3,
+        id: 22,
+        title: 'dolor sint quo a velit explicabo quia nam',
+        body:
+            'eos qui et ipsum ipsam suscipit aut\n' +
+            'sed omnis non odio\n' +
+            'expedita earum mollitia molestiae aut atque rem suscipit\n' +
+            'nam impedit esse'
+    },
+    {
+        userId: 3,
+        id: 23,
+        title: 'maxime id vitae nihil numquam',
+        body:
+            'veritatis unde neque eligendi\n' +
+            'quae quod architecto quo neque vitae\n' +
+            'est illo sit tempora doloremque fugit quod\n' +
+            'et et vel beatae sequi ullam sed tenetur perspiciatis'
+    },
+    {
+        userId: 3,
+        id: 24,
+        title: 'autem hic labore sunt dolores incidunt',
+        body:
+            'enim et ex nulla\n' +
+            'omnis voluptas quia qui\n' +
+            'voluptatem consequatur numquam aliquam sunt\n' +
+            'totam recusandae id dignissimos aut sed asperiores deserunt'
+    },
+    {
+        userId: 3,
+        id: 25,
+        title: 'rem alias distinctio quo quis',
+        body:
+            'ullam consequatur ut\n' +
+            'omnis quis sit vel consequuntur\n' +
+            'ipsa eligendi ipsum molestiae et omnis error nostrum\n' +
+            'molestiae illo tempore quia et distinctio'
+    },
+    {
+        userId: 3,
+        id: 26,
+        title: 'est et quae odit qui non',
+        body:
+            'similique esse doloribus nihil accusamus\n' +
+            'omnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\n' +
+            'perspiciatis cum ut laudantium\n' +
+            'omnis aut molestiae vel vero'
+    },
+    {
+        userId: 3,
+        id: 27,
+        title: 'quasi id et eos tenetur aut quo autem',
+        body:
+            'eum sed dolores ipsam sint possimus debitis occaecati\n' +
+            'debitis qui qui et\n' +
+            'ut placeat enim earum aut odit facilis\n' +
+            'consequatur suscipit necessitatibus rerum sed inventore temporibus consequatur'
+    },
+    {
+        userId: 3,
+        id: 28,
+        title: 'delectus ullam et corporis nulla voluptas sequi',
+        body:
+            'non et quaerat ex quae ad maiores\n' +
+            'maiores recusandae totam aut blanditiis mollitia quas illo\n' +
+            'ut voluptatibus voluptatem\n' +
+            'similique nostrum eum'
+    },
+    {
+        userId: 3,
+        id: 29,
+        title: 'iusto eius quod necessitatibus culpa ea',
+        body:
+            'odit magnam ut saepe sed non qui\n' +
+            'tempora atque nihil\n' +
+            'accusamus illum doloribus illo dolor\n' +
+            'eligendi repudiandae odit magni similique sed cum maiores'
+    },
+    {
+        userId: 3,
+        id: 30,
+        title: 'a quo magni similique perferendis',
+        body:
+            'alias dolor cumque\n' +
+            'impedit blanditiis non eveniet odio maxime\n' +
+            'blanditiis amet eius quis tempora quia autem rem\n' +
+            'a provident perspiciatis quia'
+    }
+];
