c45e6e5c528226cb27a722e6cea298246130d8bb
lekuro <lekurot@gmail.com>
2025-11-13 16:54:57 +0200

    added provider to cat image

--- lesson13-Api-integration-contract-testing/mocha/specs/cat-image.pact.spec.ts
+++ lesson13-Api-integration-contract-testing/mocha/specs/cat-image.pact.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
 import { CatService } from '../src/cat-service';
 import { IImageDto } from '../src/models/index';
-import { MatchersV3, PactV3 } from '@pact-foundation/pact';
+import { MatchersV3, PactV3, Verifier } from '@pact-foundation/pact';
+import * as path from 'path';
 
 describe('-------- cat-image api contract test --------', () => {
     const apiKey = 'live_ITUwxKsqOsSw4j3mlY0zzdlr1E2orBg2rG5Xbpmij4bdXIvLsMtZXTXzvK21LS3C';
@@ -54,6 +55,18 @@ describe('-------- cat-image api contract test --------', () => {
                 expect(images[0].width).to.be.a('number');
                 expect(images[0].height).to.be.a('number');
             });
+        });
+    });
+    describe('provider test', () => {
+        it('verify contract', () => {
+            return new Verifier({
+                providerBaseUrl: 'https://api.thecatapi.com/v1',
+                pactUrls: [path.resolve(process.cwd(), 'pacts', 'cat-consumer-cat-provider.json')]
+            })
+                .verifyProvider()
+                .then(() => {
+                    console.log('contract verified');
+                });
         });
     });
 });
