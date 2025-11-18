ad8dc75599e605bddf5098b8adb1c1ce9e58eb05
lekuro <lekurot@gmail.com>
2025-11-18 21:58:16 +0200

    tested login page

---
+++ lesson17-WebDriverIO-SeleniumWebDriver/WebDriverIO-mocha-teacher/src/page-objects/index.ts
@@ -0,0 +1,3 @@
+export * from './login.page';
+export * from './page';
+export * from './incomes.page';
---
+++ lesson17-WebDriverIO-SeleniumWebDriver/WebDriverIO-mocha-teacher/src/page-objects/login.page.ts
@@ -0,0 +1,73 @@
+import { $ } from '@wdio/globals';
+import Page from './page.js';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+export class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+
+    public get signInButton(): ChainablePromiseElement {
+        return $('.signin-button');
+    }
+
+    private get inputEmail(): ChainablePromiseElement {
+        return $('input[type="email"]');
+    }
+
+    private get inputPassword(): ChainablePromiseElement {
+        return $('input[type="password"]');
+    }
+
+    private get btnSubmit(): ChainablePromiseElement {
+        return $('button[type="submit"]');
+    }
+
+    public get errorEnterEmail(): ChainablePromiseElement {
+        return $('input[type="email"] + p');
+    }
+
+    public get errorEnterPassword(): ChainablePromiseElement {
+        return $('input[type="password"] + p');
+    }
+
+    public get errorEmailShouldContainSymbol(): ChainablePromiseElement {
+        return $('//form//*[contains(text(), "Електрона адреса має містити знак")]');
+    }
+
+    public get errorInvalidUsernameOrPassword(): ChainablePromiseElement {
+        return $('//form//*[contains(text(), "Invalid username or password")]');
+    }
+
+    public get linkForgotPassword(): ChainablePromiseElement {
+        return $('//form//*[contains(text(), "Забули пароль?")]');
+    }
+
+    public get linkSignIn(): ChainablePromiseElement {
+        return $('//form//*[contains(text(), "Зареєструватися")]');
+    }
+
+    public get pageHeader(): ChainablePromiseElement {
+        return $('//button[@aria-label="Close modal"]/../h2');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    public async login(email: string, password: string): Promise<void> {
+        await this.signInButton.click();
+        await this.inputEmail.setValue(email);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    // public open () {
+    //     return super.open('login');
+    // }
+}
