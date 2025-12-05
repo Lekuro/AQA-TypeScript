d716719d32e6ca3b79cffcb67317bd1a47698011
lekuro <lekurot@gmail.com>
2025-12-05 05:01:50 +0200

    i ca't to do redirect in new tab help please

--- lesson20-e2e-BDD-Gherkin-Cucumber-Playwright/start-from-07-TS-basic/src/steps/main.steps.ts
+++ lesson20-e2e-BDD-Gherkin-Cucumber-Playwright/start-from-07-TS-basic/src/steps/main.steps.ts
@@ -1,39 +1,65 @@
-import { DataTable, Given, Then, When } from '@cucumber/cucumber';
+import { Given, Then, When } from '@cucumber/cucumber';
 import { RobotDreamsWorld } from '../worlds/robot-dreams.world.ts';
 import { expect } from 'chai';
+import { Page } from 'playwright';
 
-Given('the user navigate to the jira home page', async function(this: RobotDreamsWorld) {
+Given('the user navigate to the main page', async function (this: RobotDreamsWorld) {
     await this.mainPage.goTo();
 });
 
-
-When('the user clicks the Get started button', async function(this: RobotDreamsWorld) {
+When('the user clicks the Get started button', async function (this: RobotDreamsWorld) {
+    const pagePromise: Promise<Page> = await this.globalContext.waitForEvent('page');
     await this.mainPage.clickGetStarted();
 });
 
-Then('the user verifies that new page is opened', function(this: RobotDreamsWorld) {
+Then('the user verifies that new page is opened', function (this: RobotDreamsWorld) {
     const context = RobotDreamsWorld.globalContext;
-    const actualOptions = (this.scenarioContext.get('menuItems') as MenuItemOption[]).map(item => item.itemName);
+    const actualOptions = (this.scenarioContext.get('menuItems') as MenuItemOption[]).map((item) => item.itemName);
 
     expect(actualOptions).to.include.members(expectedOptions);
 });
+// Assuming you have access to `page` (current page) and `context` (browser context)
+// in your Cucumber world or shared state.
 
+When('I click the link that opens a new tab', async function () {
+    // Wait for the 'page' (or 'popup') event to occur, which is triggered when a new tab opens
+    const pagePromise: Promise<Page> = this.context.waitForEvent('page');
 
-When('the user clicks the {string} menu item', async function(this: RobotDreamsWorld, menuItem: string) {
-    await this.jiraPage.sideMenuComponent.clickMenuItem(menuItem);
+    // Perform the action that opens the new tab (e.g., clicking a link with target="_blank")
+    await this.page.getByRole('link', { name: 'Open new tab' }).click();
+
+    // Await the new page object
+    this.newPage = await pagePromise;
+
+    // Wait for the new page to finish loading its initial content/redirects
+    await this.newPage.waitForLoadState();
 });
 
-Then('the customize sidebar modal should be opened', async function(this: RobotDreamsWorld) {
-    await this.jiraPage.customizeSidebarModalComponent.waitFor();
+Then('the new tab should have the URL {string}', async function (expectedUrl: string) {
+    // Use Playwright's built-in assertion with auto-waiting
+    await expect(this.newPage).toHaveURL(expectedUrl);
 });
 
-When('the user reads the customize navigation elements', async function(this: RobotDreamsWorld) {
-    const customizationElements = await this.jiraPage.customizeSidebarModalComponent.getCustomizeJiraNavigationElements();
-    this.scenarioContext.set('customizationElements', customizationElements);
+// Example using a partial URL or regex
+Then('the new tab should redirect to a URL containing {string}', async function (partialUrl: string) {
+    // You can use a regex or a glob pattern for flexibility
+    await expect(this.newPage).toHaveURL(/.*${partialUrl}.*/);
 });
+// When('the user clicks the {string} menu item', async function(this: RobotDreamsWorld, menuItem: string) {
+//     await this.jiraPage.sideMenuComponent.clickMenuItem(menuItem);
+// });
 
-Then('the following options should be present in the customize sidebar modal:', function(this: RobotDreamsWorld, data: DataTable) {
-    const actualOptions = this.scenarioContext.get('customizationElements') as string[];
-    const expectedOptions = data.hashes().map(row => row['options']);
-    expect(actualOptions).to.include.members(expectedOptions);
-});
+// Then('the customize sidebar modal should be opened', async function(this: RobotDreamsWorld) {
+//     await this.jiraPage.customizeSidebarModalComponent.waitFor();
+// });
+
+// When('the user reads the customize navigation elements', async function(this: RobotDreamsWorld) {
+//     const customizationElements = await this.jiraPage.customizeSidebarModalComponent.getCustomizeJiraNavigationElements();
+//     this.scenarioContext.set('customizationElements', customizationElements);
+// });
+
+// Then('the following options should be present in the customize sidebar modal:', function(this: RobotDreamsWorld, data: DataTable) {
+//     const actualOptions = this.scenarioContext.get('customizationElements') as string[];
+//     const expectedOptions = data.hashes().map(row => row['options']);
+//     expect(actualOptions).to.include.members(expectedOptions);
+// });
