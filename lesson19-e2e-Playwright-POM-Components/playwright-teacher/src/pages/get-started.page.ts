8c66216caf3a87e7ab119c8bf2d30791640539a3
lekuro <lekurot@gmail.com>
2025-12-05 03:15:19 +0200

    added fixture but it don't work during redirect help!!! please!!!

--- lesson19-e2e-Playwright-POM-Components/playwright-teacher/src/pages/get-started.page.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@playwright/test';
-import { JiraBasePage } from './playwright-base.page';
-
-export class JiraPage extends JiraBasePage {
-    public constructor(page: Page, url: string) {
-        super(page, url);
-    }
-}
